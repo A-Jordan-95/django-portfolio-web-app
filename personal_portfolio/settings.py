3ace3df10798be3c37a8f19db5f57c037900e629
A-Jordan-95 <arj14b@my.fsu.edu>
2020-08-19 13:17:48 -0400

    fixed errors in settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('APP_SECRET_KEY' '')
+SECRET_KEY = os.environ.get('APP_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': os.environ.get('DB_HOST', ''),
-        'NAME': os.environ.get('DB_NAME' ''),
+        'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
     }
